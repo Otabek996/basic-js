a5f6f7be5ad2c00823a7ac01325dddcb77706e28
Tuychiev Otabek <otabekotabek996996@gmail.com>
2023-12-24 02:15:25 +0500

    solve dns stats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  if (domains.length === 0) return {};
+
+  const p = `.${domains[0].split(".").slice(-1)[0]}`;
+  
+  let obj = {};
+  
+  domains.forEach(item => {
+      if (Object.keys(obj).length === 0) {
+          obj[`${p}`] = 0;
+          obj[`.${item.split(".").reverse().join(".")}`] = 0;
+      } else obj[`.${item.split(".").reverse().join(".")}`] = 0;
+  })
+  
+  let objKeys = Object.keys(obj);
+  
+  objKeys.forEach(key => {
+      domains.forEach(item => {
+          let k = key.split(".").reverse().slice(0, -1).join(".");
+          if (item.includes(k)) {
+              obj[`${key}`]++;
+          }
+      })
+  })
+  
+  return obj;
 }
 
 module.exports = {
