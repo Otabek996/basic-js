d5e2ec06e16df6a0f915735b70c43990a975b95e
Tuychiev Otabek <otabekotabek996996@gmail.com>
2023-12-25 18:01:41 +0500

    solve simple chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,25 +5,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  chains: [],
+
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.chains.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  addLink(value) {
+    if (!String(value)) value = '';
+    this.chains.push(`( ${String(value)} )`);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  removeLink(position) {
+    if (Number(position) !== +position || (position >= this.chains.length || position - 1 < 0)) {
+      this.chains = [];
+      throw new Error("You can't remove incorrect link!");
+    }
+    this.chains.splice(position - 1, 1);
+    return this;
   },
+
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.chains = this.chains.reverse();
+    return this;
   },
+
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const res = this.chains.join("~~");
+    this.chains = [];
+    return res;
   }
 };
 
