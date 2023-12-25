b6d416a2eac8b7a80f57dbff9b512d451eb3abca
Tuychiev Otabek <otabekotabek996996@gmail.com>
2023-12-26 00:55:16 +0500

    solve vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,61 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(reverse) {
+    this.reverse = reverse;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (!message || !key) throw new Error(`Incorrect arguments!`);
+    let alp = [];
+    for (let i = 65; i < 91; i++) alp.push(String.fromCharCode(i));
+    let messages = message.toUpperCase().split("");
+    let keys = key.toUpperCase().split("");
+    let res = [];
+    let counter = 0;
+    for (let i = 0; i < messages.length; i++) {
+      if (alp.indexOf(messages[i]) === -1) {
+        res.push(messages[i]);
+        continue;
+      }
+      let sum = alp.indexOf(messages[i]) + alp.indexOf(keys[counter]);
+      let index = -1;
+      if (alp.length - 1 - sum < 0) {
+        index = Math.abs(alp.length - 1 - sum) - 1;
+      } else index = sum;
+      res.push(alp[index]);
+      counter++;
+      if (counter === keys.length) counter = 0;
+    }
+
+    const finalRes = this.reverse === false ? res.reverse().join("") : res.join("");
+    return finalRes;
+  }
+
+  decrypt(message, key) {
+    if (!message || !key) throw new Error(`Incorrect arguments!`);
+    let alp = [];
+    for (let i = 65; i < 91; i++) alp.push(String.fromCharCode(i));
+    let messages = message.toUpperCase().split("");
+    let keys = key.toUpperCase().split("");
+    let res = [];
+    let counter = 0;
+    for (let i = 0; i < messages.length; i++) {
+      if (alp.indexOf(messages[i]) === -1) {
+        res.push(messages[i]);
+        continue;
+      }
+      let nm = alp.indexOf(messages[i]);
+      let nk = alp.indexOf(keys[counter]);
+      let index = 1 + (nm > nk) ? nm - nk : nk - nm;
+      if (index < 0) index = alp.length + index;
+      res.push(alp[index]);
+      counter++;
+      if (counter === keys.length) counter = 0;
+    }
+
+    const finalRes = this.reverse === false ? res.reverse().join("") : res.join("");
+    return finalRes;
   }
 }
 
